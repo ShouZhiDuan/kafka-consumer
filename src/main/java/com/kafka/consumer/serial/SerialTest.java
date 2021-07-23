8f2561a65284fa9d433bf02c3e38e550797f2ce4
段守志 <shouzhi.duan@nvxclouds.com>
2021-07-23 11:19:30 +0800

    实现XML序列技术

--- src/main/java/com/kafka/consumer/serial/SerialTest.java
+++ src/main/java/com/kafka/consumer/serial/SerialTest.java
@@ -13,19 +13,20 @@ import java.io.IOException;
 @Slf4j
 public class SerialTest {
 
+    private final static DataDTO dataDTO = new DataDTO("java",6,"浙江省杭州市滨江区");
+
     /**
      * Java序列化测试
      */
     @Test
     public void javaSerialTest() throws IOException {
-        DataDTO dataDTO = new DataDTO("java",6,"浙江省杭州市滨江区");
         ISerializer serializer = new JavaSerializer();
         //序列化  233个字节
         byte[] serialize = serializer.serialize(dataDTO);
         log.info("JAVA序列后的对象大小：" + serialize.length + "字节");
         //反序列化
         DataDTO deserialize = serializer.deserialize(serialize);
-        log.info("反序列得到的对象数据：" + deserialize);
+        log.info("JAVA反序列得到的对象数据：" + deserialize);
         log.info("完成java序列化测试");
     }
 
@@ -33,8 +34,15 @@ public class SerialTest {
      * Xml序列化测试
      */
     @Test
-    public void xmlSerialTest(){
-
+    public void xmlSerialTest() throws IOException {
+        ISerializer serializer = new XmlSerializer();
+        //序列化  144个字节
+        byte[] serialize = serializer.serialize(dataDTO);
+        log.info("XML序列后的对象大小：" + serialize.length + "字节");
+        //反序列化
+        DataDTO deserialize = serializer.deserialize(serialize);
+        log.info("XML反序列得到的对象数据：" + deserialize);
+        log.info("完成java序列化测试");
     }
 
     /**
--- src/main/java/com/kafka/consumer/serial/XmlSerializer.java
+++ src/main/java/com/kafka/consumer/serial/XmlSerializer.java
@@ -1,19 +1,30 @@
 package com.kafka.consumer.serial;
 
-import java.io.IOException;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @Auther: ShouZhi@Duan
  * @Description: XML序列、反序列方式
  */
+@Slf4j
 public class XmlSerializer implements ISerializer{
+
+    XStream stream;
+
+    public XmlSerializer(){
+        stream = new XStream(new DomDriver());
+    }
+
     @Override
-    public <T> byte[] serialize(T obj) throws IOException {
-        return new byte[0];
+    public <T> byte[] serialize(T obj) {
+        byte[] bytes = this.stream.toXML(obj).getBytes();
+        return bytes;
     }
 
     @Override
     public <T> T deserialize(byte[] data) {
-        return null;
+        return (T) this.stream.fromXML(new String(data));
     }
 }
