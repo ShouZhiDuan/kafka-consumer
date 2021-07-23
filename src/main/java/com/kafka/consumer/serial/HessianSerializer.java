c9f7c02cbf428332df4139e2815ecfd83e7f209a
段守志 <shouzhi.duan@nvxclouds.com>
2021-07-23 11:32:57 +0800

    实现

---
+++ src/main/java/com/kafka/consumer/serial/HessianSerializer.java
@@ -0,0 +1,40 @@
+package com.kafka.consumer.serial;
+
+import com.caucho.hessian.io.HessianInput;
+import com.caucho.hessian.io.HessianOutput;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+/**
+ * @Auther: ShouZhi@Duan
+ * @Description: Hessian序列、反序列方式
+ */
+public class HessianSerializer implements ISerializer{
+
+    @Override
+    public <T> byte[] serialize(T obj) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        HessianOutput hessianOutput = new HessianOutput(byteArrayOutputStream);
+        try {
+            hessianOutput.writeObject(obj);
+            return byteArrayOutputStream.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new byte[0];
+    }
+
+    @Override
+    public <T> T deserialize(byte[] data) {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(data);
+        HessianInput hessianInput = new HessianInput(byteArrayInputStream);
+        try {
+            return (T)hessianInput.readObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- src/main/java/com/kafka/consumer/serial/SerialTest.java
+++ src/main/java/com/kafka/consumer/serial/SerialTest.java
@@ -27,7 +27,7 @@ public class SerialTest {
         //反序列化
         DataDTO deserialize = serializer.deserialize(serialize);
         log.info("JAVA反序列得到的对象数据：" + deserialize);
-        log.info("完成java序列化测试");
+        log.info("JAVA序列化测试完成");
     }
 
     /**
@@ -42,15 +42,22 @@ public class SerialTest {
         //反序列化
         DataDTO deserialize = serializer.deserialize(serialize);
         log.info("XML反序列得到的对象数据：" + deserialize);
-        log.info("完成java序列化测试");
+        log.info("XML序列化测试完成");
     }
 
     /**
      * Hessian序列化测试
      */
     @Test
-    public void hessianSerialTest(){
-
+    public void hessianSerialTest() throws IOException {
+        ISerializer serializer = new HessianSerializer();
+        //序列化  97个字节
+        byte[] serialize = serializer.serialize(dataDTO);
+        log.info("HESSIAN序列后的对象大小：" + serialize.length + "字节");
+        //反序列化
+        DataDTO deserialize = serializer.deserialize(serialize);
+        log.info("HESSIAN反序列得到的对象数据：" + deserialize);
+        log.info("HESSIAN序列化测试完成");
     }
 
     /**
