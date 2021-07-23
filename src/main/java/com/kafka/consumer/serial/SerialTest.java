14f669b6d101c968c0688145b76725c7113eeeb9
段守志 <shouzhi.duan@nvxclouds.com>
2021-07-23 14:57:54 +0800

    添加protobuf和alibaba fast json序列技术

--- src/main/java/com/kafka/consumer/serial/SerialTest.java
+++ src/main/java/com/kafka/consumer/serial/SerialTest.java
@@ -1,6 +1,10 @@
 package com.kafka.consumer.serial;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.google.protobuf.ByteString;
 import com.kafka.consumer.dto.DataDTO;
+import com.kafka.consumer.proto_dto.DataDTOProto;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
@@ -13,59 +17,73 @@ import java.io.IOException;
 @Slf4j
 public class SerialTest {
 
-    private final static DataDTO dataDTO = new DataDTO("java",6,"浙江省杭州市滨江区");
+    private final static DataDTO dataDTO = new DataDTO("java", 6, "浙江省杭州市滨江区");
 
     /**
-     * Java序列化测试
+     * Java序列测试 233个字节
      */
     @Test
     public void javaSerialTest() throws IOException {
         ISerializer serializer = new JavaSerializer();
-        //序列化  233个字节
+        //序列  233个字节
         byte[] serialize = serializer.serialize(dataDTO);
         log.info("JAVA序列后的对象大小：" + serialize.length + "字节");
-        //反序列化
+        //反序列
         DataDTO deserialize = serializer.deserialize(serialize);
         log.info("JAVA反序列得到的对象数据：" + deserialize);
-        log.info("JAVA序列化测试完成");
+        log.info("JAVA序列测试完成");
     }
 
     /**
-     * Xml序列化测试
+     * Xml序列测试 144个字节
      */
     @Test
     public void xmlSerialTest() throws IOException {
         ISerializer serializer = new XmlSerializer();
-        //序列化  144个字节
+        //序列  144个字节
         byte[] serialize = serializer.serialize(dataDTO);
         log.info("XML序列后的对象大小：" + serialize.length + "字节");
-        //反序列化
+        //反序列
         DataDTO deserialize = serializer.deserialize(serialize);
         log.info("XML反序列得到的对象数据：" + deserialize);
-        log.info("XML序列化测试完成");
+        log.info("XML序列测试完成");
     }
 
     /**
-     * Hessian序列化测试
+     * Hessian序列测试 97个字节
      */
     @Test
     public void hessianSerialTest() throws IOException {
         ISerializer serializer = new HessianSerializer();
-        //序列化  97个字节
+        //序列  97个字节
         byte[] serialize = serializer.serialize(dataDTO);
         log.info("HESSIAN序列后的对象大小：" + serialize.length + "字节");
-        //反序列化
+        //反序列
         DataDTO deserialize = serializer.deserialize(serialize);
         log.info("HESSIAN反序列得到的对象数据：" + deserialize);
-        log.info("HESSIAN序列化测试完成");
+        log.info("HESSIAN序列测试完成");
     }
 
     /**
-     * ProtoBuf序列化测试
+     * FastJson序列测试 40字节
+     * 参考：https://blog.csdn.net/u010246789/article/details/52539576/
      */
     @Test
-    public void protoBufSerialTest(){
+    public void jsonSerialTest(){
+        byte[] bytes = JSON.toJSONBytes(dataDTO, SerializerFeature.BeanToArray);
+        System.out.printf(String.valueOf(bytes.length));
+    }
 
+    /**
+     * ProtoBuf序列测试 37个字节
+     */
+    @Test
+    public void protoBufSerialTest() {
+        DataDTOProto.DataDTO dataDTO = DataDTOProto.DataDTO.newBuilder().setName("java").setAge(6).setAddr("浙江省杭州市滨江区").build();
+        ByteString bs = dataDTO.toByteString();
+        //序列 37个字节
+        byte[] bytes = bs.toByteArray();
+        log.info("PROTOC序列后的对象大小：" + bytes.length + "字节");
     }
 
 
